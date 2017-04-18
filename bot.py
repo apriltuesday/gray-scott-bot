b2b3adb696394bfa8aa98880e6f731162b88fc8f
apriltuesday <april.tuesday@gmail.com>
2017-04-18 16:36:15 +0100

    don't generate images on retweets

--- bot.py
+++ bot.py
@@ -17,21 +17,27 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 PIC_FILE = 'tmp.jpg'
+BOT_NAME = 'GrayScottBot'
 
 
 class GrayScottBot(tweepy.StreamListener):
 
 	def on_status(self, status):
-		# TODO don't do retweets!!!
 		username = status.author.screen_name
-		if username == 'GrayScottBot':
+		if username == BOT_NAME:
 			return
+		# stop if someone just retweeted you, gets obnoxious
+		if (hasattr(status, 'retweeted_status')
+			and status.retweeted_status != None
+			and status.retweeted_status.author.screen_name == BOT_NAME):
+			return
+
 		print 'status received'
 		try:
 			media = status.entities['media'][0]
 			if media['type'] == 'photo':
 				img_url = media['media_url']
-		except: #get profile pic if no image in DM
+		except: #get profile pic if no image in message
 			img_url = api.get_user(username).profile_image_url_https
 			img_url = ''.join(img_url.split('_normal'))
 		self.direct_message(username, img_url)
@@ -66,7 +72,7 @@ class GrayScottBot(tweepy.StreamListener):
 			max_f = sample_feed()
 			kill = sample_kill(max_f)
 			feed = max_f * self.get_feed_matrix_from_image(img_url)
-			message = '@{} f={}, k={}'.format(username, max_f, kill)
+			message = 'f={}, k={} @{}'.format(max_f, kill, username)
 			print message
 			output_file = generate_image(feed, kill)
 			api.update_with_media(output_file, message)
@@ -79,7 +85,7 @@ def main():
 	bot = GrayScottBot()
 
 	stream = tweepy.Stream(auth, bot)
-	stream.userstream()
+	stream.userstream(_with='user')
 
 
 if __name__ == '__main__':
